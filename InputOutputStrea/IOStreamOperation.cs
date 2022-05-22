da6e3140366c6b2358f50a1c51c27b154ea8007a
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-22 12:09:13 +0530

    [ vinay] Add: Reading data texts by texts from the Contents File

--- InputOutputStrea/IOStreamOperation.cs
+++ InputOutputStrea/IOStreamOperation.cs
@@ -18,5 +18,18 @@ namespace InputOutputStrea
             }
             File.Create(filePath);
         }
+        public void ReadAllLines()
+        {
+            string[] array = File.ReadAllLines(filePath);
+            foreach (string line in array)
+            {
+                Console.WriteLine("All lines are:->" + line);
+            }
+        }
+        public void ReadAllText()
+        {
+            string text = File.ReadAllText(filePath);
+            Console.WriteLine("Text are Presen in the File:->" + text);
+        }
     }
 }
--- InputOutputStrea/Program.cs
+++ InputOutputStrea/Program.cs
@@ -9,7 +9,7 @@ public class Program
         IOStreamOperation operation = new IOStreamOperation();
         while (check)
         { 
-        Console.WriteLine(" Checking File is Exists or not 1\n");
+        Console.WriteLine(" Checking File is Exists or not 1\n Reading Line by Line From File 2\n Reading Text From File 3\n");
             Console.WriteLine("$$$$$$$$$$$$$ Enter the Above Option $$$$$$$$$$$$$");
             int option=Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -20,7 +20,13 @@ public class Program
                 case 0:
                     check=false;
                     break;
-                    default:
+                case 2:
+                   operation.ReadAllLines();
+                    break;
+                case 3:
+                    operation.ReadAllText();
+                    break;
+                default:
                     Console.WriteLine("Enter the Valid Number to Perform the Specific Operation");
                     break;
             }
