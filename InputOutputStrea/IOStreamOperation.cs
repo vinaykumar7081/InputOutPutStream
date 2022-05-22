62a434459950d8ac3049f10c321c667b99d710de
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-22 11:34:20 +0530

    [VINAY] Add: Checking the File Existing or not

---
+++ InputOutputStrea/IOStreamOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOutputStrea
+{
+    public class IOStreamOperation
+    {
+        string filePath =@"D:\InputOutputProblem\InputOutPutStream\InputOutputStrea\ContentsFile.txt";
+        public void FileExists()
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File is Exists");
+                return;
+            }
+            File.Create(filePath);
+        }
+    }
+}
---
+++ InputOutputStrea/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using InputOutputStrea;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("                >>>>>>>>>>>>>>>>>>>> WELCOME IN THE I/O STREAM <<<<<<<<<<<<<<<<<<<<<<");
+        bool check=true;
+        IOStreamOperation operation = new IOStreamOperation();
+        while (check)
+        { 
+        Console.WriteLine(" Checking File is Exists or not 1\n");
+            Console.WriteLine("$$$$$$$$$$$$$ Enter the Above Option $$$$$$$$$$$$$");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            { 
+                case 1:
+                    operation.FileExists();
+                break;
+                case 0:
+                    check=false;
+                    break;
+                    default:
+                    Console.WriteLine("Enter the Valid Number to Perform the Specific Operation");
+                    break;
+            }
+        }
+        operation.FileExists();
+    }
+}
